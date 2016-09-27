6a64ef8089371c2f5306af6310bb4de068c48ad7
YounggilLee <clickbabo@gmail.com>
2016-09-27 12:22:24 -0400

    first successes !!

--- src/assign1_younggil_scene/FXMLDocumentController.java
+++ src/assign1_younggil_scene/FXMLDocumentController.java
@@ -8,9 +8,12 @@ package assign1_younggil_scene;
 import java.net.URL;
 import java.sql.*;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -31,85 +34,104 @@ import javafx.stage.Stage;
  */
 public class FXMLDocumentController implements Initializable {
 
-    @FXML    private TextField url;
-    @FXML    private TextField user;
-    @FXML    private TextField password;
-    @FXML    private Button button;
-    @FXML    private Stage stage;
-    @FXML    private RadioButton p1;
-    @FXML    private RadioButton d2;
-    @FXML    private RadioButton m3;
-    @FXML    private TableColumn <Student, String> columnId;
-    @FXML    private TableColumn <Student, String> columnFirstName;
-    @FXML    private TableColumn <Student, String> columnLastName;
+    @FXML
+    private TextField url;
+    @FXML
+    private TextField user;
+    @FXML
+    private TextField password;
+    @FXML
+    private Button button;
+    @FXML
+    private Stage stage;
+    @FXML
+    private RadioButton p1;
+    @FXML
+    private RadioButton d2;
+    @FXML
+    private RadioButton m3;
+    @FXML
+    private TableColumn<Student, String> columnId;
+    @FXML
+    private TableColumn<Student, String> columnFirstName;
+    @FXML
+    private TableColumn<Student, String> columnLastName;
     String address;
     String id;
     String pass;
     boolean result;
     JdbcHelper jdbc = new JdbcHelper();
-    TableView<Student> tableStudent;
+    @FXML private TableView<Student> tableStudent;
+    ArrayList<Student> students = new ArrayList<>();
+    
+    Connection conn;
+    Statement stmt;
+    ResultSet rs;
+
 //jdbc:mysql://localhost:3306/ejd
-    private void handleButtonAction(ActionEvent event) throws SQLException {
-        
-        this.address = url.getText();
-        this.id = user.getText();
-        this.pass = password.getText();
-        result =  jdbc.connect("jdbc:mysql://localhost:3306/ejd","root","1234");
-        //result =  jdbc.connect(url.getText(),user.getText(),password.getText());  
-        System.out.println("connected!! ");
-        url.clear();
-        user.clear();
-        password.clear();
+  @FXML
+  private void handleButtonAction(ActionEvent event) throws SQLException {
 
-        
-    }
-
-    public void printResult(ResultSet resultSet) {
-        
-        try {
-            while (resultSet.next()) {
-                columnId.setId(resultSet.getString(1));
-                columnFirstName.setId(resultSet.getString(2));
-                columnLastName.setId(resultSet.getString(3));
-                     tableStudent.getColumns().addAll(columnId,columnFirstName,columnLastName );
-                        // tableStudent.add(new Student(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3)));
-                    
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(JdbcHelper.class.getName()).log(Level.SEVERE, null, ex);
-        }
+       // this.address = url.getText();
+        //this.id = user.getText();
+       // this.pass = password.getText();
+        //result =  jdbc.connect(url.getText(),user.getText(),password.getText());
+       // result = jdbc.connect("jdbc:mysql://localhost:3306/ejd", "root", "1234");
        
+       try {
+            this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ejd", "root", "1234");
+            this.stmt = conn.createStatement();
+            System.out.println("connection ok");
+           
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+                     
+        }
+        System.out.println("connected!! ");
+       
+
     }
-    
-     private void handleRadioAction(ActionEvent event){
-          String query1 = "select id, firstname, lastName from student inner join CourseStudent \n" +
-                            "on student.id = CourseStudent.studentId where CourseStudent.courseId = 'PROG10000'";
-          String sql2 = "insert into Student ( id, firstName, lastName)values(?,?,?)";
-                   
-         
-          if(event.getSource() == p1){ 
-              System.out.println("p1 selected!!");
-            //   ResultSet rs = jdbc.query(query1);
-             //  printResult(rs); 
-          }
-          if(event.getSource() == d2){ 
-              System.out.println("d2 selected!!");
-              // ResultSet rs = jdbc.query(query1);
-             //  printResult(rs); 
-          }
-          if(event.getSource() == m3){ 
-              System.out.println("m3 selected!!");
-              // ResultSet rs = jdbc.query(query1);
-              // printResult(rs); 
-          }
-     }
-    
+  @FXML
+    private void handleRadioAction(ActionEvent event) {
+        
+        System.out.println("p1 is selected!!!");
+        String query1 = "select id, firstname, lastName from student inner join CourseStudent \n"
+                + "on student.id = CourseStudent.studentId where CourseStudent.courseId = 'PROG10000'";
+
+        
+            try {
+
+              //  ResultSet rs = jdbc.query(query1);
+              ResultSet rs = stmt.executeQuery(query1);
+                while (rs.next()) {
+                    String id = rs.getString(1);                    
+                    String firstName = rs.getString(2);
+                    String lastName = rs.getString(3);
+                    System.out.println(id+firstName+lastName);
+                    Student std = new Student(id, firstName, lastName);
+                    System.out.println(std.getId() + std.getFirstName());
+                    students.add(std);
+                    System.out.println(students.get(0));
+                   // students.add(new Student(id, firstName, lastName));
+                    
+                    
+                    tableStudent.setItems(FXCollections.observableArrayList(students));
+                    columnId.setCellValueFactory(new PropertyValueFactory<Student, String>("id"));
+                    columnFirstName.setCellValueFactory(new PropertyValueFactory<Student, String>("firstName"));
+                    columnLastName.setCellValueFactory(new PropertyValueFactory<Student, String>("lastName"));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+               
+            }
+        
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        columnId.setCellValueFactory(new PropertyValueFactory<Student, String> ("id"));
-        columnFirstName.setCellValueFactory(new PropertyValueFactory<Student, String> ("firstName"));
-        columnLastName.setCellValueFactory(new PropertyValueFactory<Student, String> ("lastName"));
 
         button.setOnAction(event -> {
             try {
@@ -118,19 +140,10 @@ public class FXMLDocumentController implements Initializable {
                 System.out.println(ex.getMessage());
             }
         });
-        
-        p1.setOnAction(event -> {        
-                handleRadioAction(event);           
-        });
-        
-        d2.setOnAction(event -> {        
-                handleRadioAction(event);           
-        });
-        
-        m3.setOnAction(event -> {        
-                handleRadioAction(event);           
-        });
-        
+//        p1.setOnAction(event -> {
+//            handleRadioAction(event);
+//        });
+
     }
 
 }
