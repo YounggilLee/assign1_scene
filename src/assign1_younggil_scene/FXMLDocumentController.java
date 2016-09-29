708b392bc7839658dcaac43c0527d2c65b021021
YounggilLee <clickbabo@gmail.com>
2016-09-29 19:39:53 -0400

    adding stage close function.

--- src/assign1_younggil_scene/FXMLDocumentController.java
+++ src/assign1_younggil_scene/FXMLDocumentController.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assign1_younggil_scene;
 
 import java.net.URL;
 import java.sql.*;
-
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
 import javafx.collections.FXCollections;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
 import javafx.fxml.Initializable;
-
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
@@ -26,6 +16,10 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
+import ejd.JdbcHelper;
+import java.util.Optional;
+import javafx.application.Platform;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 /**
@@ -42,7 +36,6 @@ public class FXMLDocumentController implements Initializable {
     private TextField password;
     @FXML
     private Button button;
-    
     @FXML
     private RadioButton program;
     @FXML
@@ -55,90 +48,100 @@ public class FXMLDocumentController implements Initializable {
     private TableColumn<Student, String> columnFirstName;
     @FXML
     private TableColumn<Student, String> columnLastName;
-    String address;
-    String id;
-    String pass;
-    boolean result;
+    @FXML
+    private TableView<Student> tableStudent;
+    private String address;
+    private String id;
+    private String pass;
+    private boolean result;
     JdbcHelper jdbc = new JdbcHelper();
-    @FXML  private TableView<Student> tableStudent;
     ArrayList<Student> students = new ArrayList<>();
+    private Stage stage;
 
-//jdbc:mysql://localhost:3306/ejd
     @FXML
     private void handleButtonAction(ActionEvent event) throws SQLException {
+        
+            this.address = url.getText();
+            this.id = user.getText();
+            this.pass = password.getText();
+            
+            result = jdbc.connect(address, id, pass);
+            if (result) {               
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "DataBase is connected!! ");
+                alert.showAndWait();
+            }else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Please check log-in information!! ");
+                alert.showAndWait();
+            }
+        
 
-       this.address = url.getText();
-       this.id = user.getText();
-      this.pass = password.getText();
-       //result =  jdbc.connect(url.getText(),user.getText(),password.getText());
-        result = jdbc.connect("jdbc:mysql://localhost:3306/ejd", "root", "1234");
-        if (result) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "DataBase is connected!! ");
-            alert.showAndWait();
-        }
 
     }
 
     @FXML
     private void handleRadioAction(ActionEvent event) {
         int radioNumber = 0;
-        String query = null;
 
-        String qry1 = "select id, firstname, lastName from student inner join CourseStudent \n"
-                + "on student.id = CourseStudent.studentId where CourseStudent.courseId = ?";//'PROG10000'
-//        String qry2 = "select id, firstname, lastName from student inner join CourseStudent \n"
-//                + "on student.id = CourseStudent.studentId where CourseStudent.courseId = 'DBAS20000'";
-//        String qry3 = "select id, firstname, lastName from student inner join CourseStudent \n"
-//                + "on student.id = CourseStudent.studentId where CourseStudent.courseId = 'MATH30000'";
+        String query = "select id, firstname, lastName from student inner join CourseStudent \n"
+                + "on student.id = CourseStudent.studentId where CourseStudent.courseId = ?";
 
         try {
             if (program.isSelected()) {
                 students.clear();
                 radioNumber = 1;
-               // query = qry1;
-
-                System.out.println("pro is selected");
             }
             if (dataBase.isSelected()) {
                 students.clear();
                 radioNumber = 2;
-               // query = qry2;
-
-                System.out.println("database is selected");
             }
             if (math.isSelected()) {
                 students.clear();
                 radioNumber = 3;
-              //  query = qry3;
-
-                System.out.println("math is selected");
             }
-            ResultSet rs = jdbc.query(radioNumber,qry1);
+            ResultSet rs = jdbc.query(radioNumber, query);
 
             while (rs.next()) {
                 String id = rs.getString(1);
                 String firstName = rs.getString(2);
                 String lastName = rs.getString(3);
                 students.add(new Student(id, firstName, lastName));
-                
+
             }
-//             System.out.println("=================================");
-//            System.out.println("   Id  | First Name  | Last Name  ");
-//            System.out.println("==================================");
-//            for (Student std : students) {
-//                System.out.printf("%5s %11s %11d%s", std.getId(), std.getFirstName(), std.getLastName());
-//                            }
-//            System.out.println("=============================");
+
             tableStudent.setItems(FXCollections.observableArrayList(students));
             columnId.setCellValueFactory(new PropertyValueFactory<Student, String>("id"));
             columnFirstName.setCellValueFactory(new PropertyValueFactory<Student, String>("firstName"));
             columnLastName.setCellValueFactory(new PropertyValueFactory<Student, String>("lastName"));
 
         } catch (Exception e) {
-            ;
+
             e.printStackTrace();
         }
 
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+        stage.setOnCloseRequest(e -> {
+             
+           
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "DataBase will be disconnected!! ");
+                alert.showAndWait();
+                 if (jdbc != null) jdbc.disconnect(); 
+               Platform.exit();                       
+                        
+//            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you really want to quit?");
+//            Optional<ButtonType> result = alert.showAndWait(); 
+//            if (result.isPresent() && result.get() == ButtonType.OK) {
+//                                
+//                if (jdbc != null) jdbc.disconnect();
+//                
+//                Platform.exit();
+//            } else 
+//                // do nothing 
+//                e.consume(); // ignore the close event  
+        });
+        
     }
 
     @Override
