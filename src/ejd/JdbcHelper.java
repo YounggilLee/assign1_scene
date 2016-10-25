6a3cc5f3d39988c03d281c4f53c1543666ac5c6d
YounggilLee <clickbabo@gmail.com>
2016-10-25 15:16:41 -0400

    version3- update jdbc helper class to get anytype of input and change input type to object type arrayList.

--- src/ejd/JdbcHelper.java
+++ src/ejd/JdbcHelper.java
@@ -1,139 +1,416 @@
+
 package ejd;
 
-/** 
- * Author:  Younggil Lee
- * Student ID: 991 395 505
- * Description: Make connection or disconnection between java and database 
- **/
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-import java.sql.*;
 
-public class JdbcHelper {
-    // instance variables
+
+public class JdbcHelper
+{
+    // instance vars 
     private Connection connection;
     private Statement statement;
     private ResultSet resultSet;
-    private int updateCount = 0;
+    private String errorMessage;    // current error message
+
+    // for prepared statement
+    private String activeSql;       // parametric SQL statement
+    private PreparedStatement activeStatement;
     
-    //no-arg ctor: init instance vars
-    public JdbcHelper() {
+
+
+    // ctor
+  
+    public JdbcHelper()
+    {
+        // init instance vars
         connection = null;
         statement = null;
         resultSet = null;
+        errorMessage = "";
+        
+        activeSql = "";
+        activeStatement = null;
     }
 
-    public boolean connect(String url, String user, String pass) {
-        
-        //default value
-        boolean connected = false;
-        
-        //load Jdbc Driver
-        initJdbcDriver(url);
-        
-        try {
-            this.connection = DriverManager.getConnection(url, user, pass);
-            this.statement = connection.createStatement();
-            connected = true;
-        } catch (SQLException e) {
-            System.err.println(e.getSQLState() + ":" + e.getMessage());
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
+
+
+    
+    // try to connect to DB with 3 params (URL, user, pass) and create static
+    // statement object for statc SQL
+    // if successful, return true. Otherwise, return false
+   
+    public boolean connect(String url, String user, String pass)
+    {
+        boolean connected = false;  // return value
+        errorMessage = "";          // reset prev error message
+
+        // validate input
+        if(url == null || url.isEmpty())
+        {
+            errorMessage = "URL is null or empty in connect().";
+            System.err.println(errorMessage);
+            return connected;
+        }
+        if(user == null || user.isEmpty())
+        {
+            errorMessage = "Username in connect() cannot be null or empty.";
+            System.err.println(errorMessage);
+            return connected;
+        }
+        if(pass == null || pass.isEmpty())
+        {
+            errorMessage = "Password in connect() cannot be null or empty.";
+            System.err.println(errorMessage);
+            return connected;
         }
 
+        try
+        {
+            // try to connect
+            initJdbcDriver(url); // load jdbc driver first
+            connection = DriverManager.getConnection(url, user, pass);
+
+            // create statement object to store static SQL statements
+            statement = connection.createStatement();
+
+            connected = true;
+        }
+        catch(SQLException e)
+        {
+            connection = null;
+            errorMessage = e.getSQLState() + ": " + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        catch(Exception e)
+        {
+            connection = null;
+            errorMessage = e.getMessage();
+            System.err.println(errorMessage);    
+        }
         return connected;
     }
-    
-    // disconnect resultSet, Statment,Connection using a close method
-    public void disconnect() {
-         try { resultSet.close(); } catch (Exception e) { }
-        try { statement.close(); } catch (Exception e) { } 
-        try { connection.close(); } catch (Exception e) { }
-    }
-    
-    //execute query. such as SELECT command
-    //it returns ResultSet. IF failed, reuturn null
-    public ResultSet query(int radioNumber, String sql) {
-        resultSet = null;
-        try {
-             if(connection == null || connection.isClosed()){
-                 System.err.println(" No connection established in query() yet");
-                 return resultSet;
-             }
-             
-             // execute 
-            PreparedStatement ps = connection.prepareStatement(sql);
-            
-            // check user input and select course
-            // set string course name to ?
-            if (radioNumber == 1) {
-                ps.setString(1, "PROG10000");
-            } else if (radioNumber == 2) {
-                ps.setString(1, "DBAS20000");
-            } else {
-                ps.setString(1, "MATH30000");
-            }
-            
-            // store return value to resultset 
-            resultSet = ps.executeQuery();
-        } catch (SQLException ex) {
 
-            System.out.println(ex.getMessage());
+
+
+    
+    // clear JDBC recources
+    // It simply ignores any error generated by close()
+    
+    public void disconnect()
+    {
+        activeSql = "";
+        try{ resultSet.close(); }       catch(Exception e){}
+        try{ statement.close(); }       catch(Exception e){}
+        try{ activeStatement.close(); } catch(Exception e){} // for preparedstatement
+        try{ connection.close(); }      catch(Exception e){}
+    }
+
+
+
+   
+    // execute static SQL query statement
+    // it ruturns ResultSet object if succesful. Otherwise, returns null.
+   
+    public ResultSet query(String sql)
+    {
+        // reset return value
+        resultSet = null;
+        errorMessage = "";
+
+        // validate input
+        if(sql == null || sql.isEmpty())
+        {
+            errorMessage = "SQL string in query() cannot be null or empty.";
+            System.err.println(errorMessage);
+            return resultSet;
         }
 
+        try
+        {
+            // check connection first
+            if(connection == null || connection.isClosed())
+            {
+                errorMessage = "Connection is NOT established yet. Connect to DB first before using query()";
+                return resultSet;
+            }
+
+            resultSet = statement.executeQuery(sql);
+        }
+        catch(SQLException e)
+        {
+            errorMessage = e.getSQLState() + ": " + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        catch(Exception e)
+        {
+            errorMessage = e.getMessage();
+            System.err.println(errorMessage);
+        }
         return resultSet;
     }
-    
-    //execute update for  CREATE, INSERT INTO, UPDATE, 
-    //etc comn which does return ResultSet. If successful, 
-    //return 0 or postive number otherwise, return -1 
-    public int update(String sql) {
-        
-          // default return value
-        int result = -1;
-        
-       // validate connection
-        try {
-             if(connection == null || connection.isClosed()){
-                 System.err.println(" No connection established yet");
-                 return result;
-             }     
-             
-             //excute query
-            result = statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            System.err.println(e.getSQLState() + ":" + e.getMessage());
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
+
+
+
+   
+    // execute query for prepared statement
+   
+    public ResultSet query(String sql, ArrayList<Object> params)
+    {
+        // reset return value
+        resultSet = null;
+        errorMessage = "";
+
+        // validate input
+        if(sql == null || sql.isEmpty())
+        {
+            errorMessage = "SQL string in query() cannot be null or empty.";
+            System.err.println(errorMessage);
+            return resultSet;
         }
-      return result;   
-    }    
-    
-    
-    
-       private void initJdbcDriver(String url) {
-        // test various databases
-        try {
-            if (url.contains("jdbc:mysql")) {
-                Class.forName("com.mysql.jdbc.Driver");
-            } else if (url.contains("jdbc:oracle")) {
-                Class.forName("oracle.jdbc.OracleDriver");
-            } else if (url.contains("jdbc:derby")) {
-                Class.forName("org.apache.derby.jdbc.ClientDriver");
-            } else if (url.contains("jdbc:db2")) {
-                Class.forName("com.ibm.db2.jcc.DB2Driver");
-            } else if (url.contains("jdbc:postgresql")) {
-                Class.forName("org.postgresql.Driver");
-            } else if (url.contains("jdbc:sqlite")) {
-                Class.forName("org.sqlite.JDBC");
-            } else if (url.contains("jdbc:sqlserver")) {
-                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            } else if (url.contains("jdbc:sybase")) {
-                Class.forName("sybase.jdbc.sqlanywhere.IDriver");
+
+        try
+        {
+            // check connection first
+            if(connection == null || connection.isClosed())
+            {
+                errorMessage = "Connection is NOT established yet. Connect to DB first before using query()";
+                System.err.println(errorMessage);
+                return resultSet;
             }
-            // add other databases here
-        } catch (ClassNotFoundException e) {
-            System.err.println(e.getMessage());
+
+            // create new prepared statement only if SQL string was changed
+            if(!activeSql.equals(sql))
+            {
+                activeSql = sql;
+                activeStatement = connection.prepareStatement(sql);
+            }
+
+            // set all parameters for prepared statement
+            setParametersForPreparedStatement(params);
+
+            resultSet = activeStatement.executeQuery();
+        }
+        catch(SQLException e)
+        {
+            errorMessage = e.getSQLState() + ": " + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        catch(Exception e)
+        {
+            errorMessage = e.getMessage();
+            System.err.println(errorMessage);
+        }
+        return resultSet;
+    }
+
+
+
+   
+    // execute update that returns no ResultSet, such as CREATE, UPDATE, etc.
+    // if successful, it returns 0 or # of rows changed.
+    // if failed, it returns -1
+  
+    public int update(String sql)
+    {
+        // reset return value
+        int result = -1; // default return value
+        errorMessage = "";
+
+        // validate input
+        if(sql == null || sql.isEmpty())
+        {
+            errorMessage = "SQL string in update() cannot be null or empty.";
+            System.err.println(errorMessage);
+            return result;
+        }
+
+        try
+        {
+            // check connection first
+            if(connection == null || connection.isClosed())
+            {
+                errorMessage = "Database is NOT connected. Connect to DB before using update().";
+                System.err.println(errorMessage);
+                return result;
+            }
+
+            result = statement.executeUpdate(sql);
+        }
+        catch(SQLException e)
+        {
+            errorMessage = e.getSQLState() + ": " + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        catch(Exception e)
+        {
+            errorMessage = e.getMessage();
+            System.err.println(errorMessage);
+        }
+
+        // return result of executeUpdate()
+        return result;
+    }
+
+
+
+  
+    // execute update for preparedstatement
+    // if successful, it returns 0 or # of rows changed.
+    // if failed, it returns -1
+  
+    public int update(String sql, ArrayList<Object> params)
+    {
+        // reset return value
+        int result = -1; // default return value
+        errorMessage = "";
+
+        // validate input
+        if(sql == null || sql.isEmpty())
+        {
+            errorMessage = "SQL string in update() cannot be null or empty.";
+            System.err.println(errorMessage);
+            return result;
+        }
+
+        try
+        {
+            // check connection first
+            if(connection == null || connection.isClosed())
+            {
+                errorMessage = "Database is NOT connected. Connect to DB before using update().";
+                System.err.println(errorMessage);
+                return result;
+            }
+
+            // compare sql string with the prev sql
+            // if it is changed, create new prepared statement
+            if(!activeSql.equals(sql))
+            {
+                activeStatement = connection.prepareStatement(sql);
+                activeSql = sql; // remember the current sql
+            }
+
+            // set params for prepared statement
+            setParametersForPreparedStatement(params);
+
+            // finally execute sql
+            result = activeStatement.executeUpdate();
+        }
+        catch(SQLException e)
+        {
+            errorMessage = e.getSQLState() + ": " + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        catch(Exception e)
+        {
+            errorMessage = e.getMessage();
+            System.err.println(errorMessage);
+        }
+
+        // return result of executeUpdate()
+        return result;
+    }
+
+
+
+
+    // set parameters for prepared statement, setXXX()
+    //It will cast each param to the original data type before calling setXXX()
+  
+    private void setParametersForPreparedStatement(ArrayList<Object> params)
+    {
+        // reset
+        errorMessage = "";
+        Object param = null;    // parameter element in the list
+
+        // check params: if null, do nothing 
+        if(params == null)
+            return;
+
+        try
+        {
+            for(int i = 0; i < params.size(); ++i)
+            {
+                param = params.get(i);
+                if(param instanceof Integer)
+                    activeStatement.setInt(i+1, (Integer)param);
+                else if(param instanceof Double)
+                    activeStatement.setDouble(i+1, (Double)param);
+                else if(param instanceof String)
+                    activeStatement.setString(i+1, (String)param);
+                //@@ add more types here
+            }
+        }
+        catch(SQLException e)
+        {
+            errorMessage = e.getSQLState() + ": " + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        catch(Exception e)
+        {
+            errorMessage = e.getMessage();
+            System.err.println(errorMessage);
         }
     }
-    
+
+
+
+   
+    // return the current error message
+   
+    public String getErrorMessage()
+    {
+        return errorMessage;
+    }
+
+  // tho check jdbc connection
+     public Connection getConnection() {
+        return connection;
+    }
+
+   
+    // register the JDBC driver based on URL
+   
+    private void initJdbcDriver(String url)
+    {
+        // test various databases
+        try
+        {
+            if(url.contains("jdbc:mysql"))
+                Class.forName("com.mysql.jdbc.Driver");
+
+            else if(url.contains("jdbc:oracle"))
+                Class.forName("oracle.jdbc.OracleDriver");
+
+            else if(url.contains("jdbc:derby"))
+                Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+            else if(url.contains("jdbc:db2"))
+                Class.forName("com.ibm.db2.jcc.DB2Driver");
+
+            else if(url.contains("jdbc:postgresql"))
+                Class.forName("org.postgresql.Driver");
+
+            else if(url.contains("jdbc:sqlite"))
+                Class.forName("org.sqlite.JDBC");
+
+            else if(url.contains("jdbc:sqlserver"))
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            else if(url.contains("jdbc:sybase"))
+                Class.forName("sybase.jdbc.sqlanywhere.IDriver");
+        }
+        catch(ClassNotFoundException e)
+        {
+            errorMessage = "Failed to initialize JDBC driver class.";
+            System.err.println(errorMessage);
+        }
+    }
 }
